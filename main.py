b6adfbdfe83d47278e0cb827d7056dadf44b0d83
extr3mis <37597293+extr3mis@users.noreply.github.com>
2020-06-03 18:25:26 +0530

    Rewrite main for better functionality.

--- main.py
+++ main.py
@@ -5,37 +5,34 @@ import asyncio
 import aiohttp
 from discord.ext.commands import Bot
 import os
+import datetime
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-# bot is a subclass of client
-bot = Bot(command_prefix='!') #Change it if you want to
+class bot(Bot): #creating subclass for async implementation
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs) #inheriting
+    async def on_ready(self): 
+        print('Logged in as') #Check for login
+        self.session = aiohttp.ClientSession() #making ClientSession()
+        print(bot.user.name)
+        print(bot.user.id)
+        print('------')
 
-@bot.event
-async def on_ready(): 
-    print('Logged in as') #Check for login
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-
-@bot.command(aliases = ['x'])
+bot = bot(command_prefix = "!") #creating instance of bot
+@bot.command(aliases=['x'])
 async def xkcd(ctx,num=None,**kwargs): 
-    """Posts the XKCD of the given number, if no number is given, posts the latest."""
-    if num == None:
-        num = ''
-    else:
-        num=num+'/'
-    async with aiohttp.ClientSession() as session: #Making Clientsession
-        async with session.get(f'https://xkcd.com/{num}info.0.json') as resp:
+        """Posts the XKCD of the given number, if no number is given, posts the latest."""
+        if num is None:
+            url = 'https://xkcd.com/info.0.json'
+        else:
+            url = f'https://xkcd.com/{num}/info.0.json'
+        async with bot.session.get(url) as resp: #getting data
             print(resp.status)
-            data = await resp.text()
-            data = json.loads(data)
-            img_url = data['img'] #Pulling data
-            title = data['safe_title']
-            num = str(data['num'])
-        embed = discord.Embed(title=title, color = 0x000000) #Because black is nice.
-        embed.set_image(url = img_url)
-        embed.add_field(name='Number',value=num)
-        await ctx.send(embed = embed)
-# Token here
+            data = await resp.json() #Pulling data
+        em = discord.Embed(title=data['safe_title'], color = 0x000000, timestamp=datetime.datetime.now()) #Because black is nice.
+        em.set_image(url = data['img']) #making embed
+        em.add_field(name='Number',value=str(data['num']))
+        em.set_footer(text=f'Requested by {ctx.message.author.display_name}')
+        await ctx.send(embed = em)
 bot.run(TOKEN)
 #Made by @Extr3mis#9663
