223652ca16c9bd186d917e83535d7f458cbc9d9c
extr3mis <37597293+extr3mis@users.noreply.github.com>
2020-06-21 17:26:00 +0530

    Rewrite main.... again.
    When will I ever let go?

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import json
 import asyncio
 import aiohttp
 from discord.ext.commands import Bot
+from discord.ext import menus
 import os
 import datetime
+import random
 TOKEN = os.getenv("DISCORD_TOKEN")
-
-
+left = '⬅️'
+right = '➡️'
+rand = '🔀'
 class bot(Bot): #creating subclass for async implementation
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs) #inheriting
@@ -18,25 +21,63 @@ class bot(Bot): #creating subclass for async implementation
         print(bot.user.name)
         print(bot.user.id)
         print('------')
-
 bot = bot(command_prefix = "!") #creating instance of bot
-@bot.command(aliases=['x'])
-async def xkcd(ctx,num=None,**kwargs): 
-        """Posts the XKCD of the given number, if no number is given, posts the latest."""
-        if num is None:
-            url = 'https://xkcd.com/info.0.json'
-        else:
-            url = f'https://xkcd.com/{num}/info.0.json'
-        async with bot.session.get(url) as resp: #getting data
+
+class Menu(menus.Menu): 
+#Menu required for reaction buttons.
+    def __init__(self,num,ctx):
+        super().__init__()
+        self.num = num
+        self.ctx = ctx
+    async def send_initial_message(self,channel,ctx):
+        return await channel.send(embed = await getcomic(self.ctx,self.num))
+    @menus.button(left)
+    async def left(self,payload):
+        if self.num is None:
+            self.num=await latest()
+        self.num=int(self.num)-1
+        await self.message.edit(embed = await getcomic(self.ctx,self.num))
+    @menus.button(right)
+    async def right(self,payload):
+        if self.num is None:
+            self.num=(await latest())-1
+        self.num=int(self.num)+1
+        await self.message.edit(embed = await getcomic(self.ctx,self.num))
+    @menus.button(rand)#random comic
+    async def rand(self,payload):
+        async with bot.session.get('https://xkcd.com/info.0.json') as resp: #getting data
             print(resp.status)
             data = await resp.json() #Pulling data
-        num = data['num']
-        alt = data['alt']
-        title = data['safe_title']
-        desc = f'{alt} Link to the original [here](https://xkcd.com/{num}).'
-        em = discord.Embed(title=f'{title}: #{num}', color = 0x000000, timestamp=datetime.datetime.now(), description = desc) #Because black is nice.
-        em.set_image(url = data['img']) #making embed
-        em.set_footer(text=f'Requested by {ctx.message.author.display_name}')
-        await ctx.send(embed = em)
+        self.num = random.randint(1,int(data['num']))
+        await self.message.edit(embed = await getcomic(self.ctx,self.num))
+
+@bot.command(aliases=['x']) #command acceptor
+async def xkcd(ctx,num=None,**kwargs): 
+    """Posts the XKCD of the given number, if no number is given, posts the latest."""
+    m = Menu(num,ctx)
+    await m.start(ctx)
+
+async def getcomic(ctx,num=None): #async func to get comic
+    if num is None:
+        num = await latest()
+    async with bot.session.get(f'https://xkcd.com/{num}/info.0.json') as resp: #getting data
+        print(resp.status)
+        data = await resp.json() #Pulling data
+    num = data['num']
+    alt = data['alt']
+    title = data['safe_title']
+    desc = f'{alt} Link to the original [here](https://xkcd.com/{num}).'
+    em = discord.Embed(title=f'{title}: #{num}', color = 0x000000, timestamp=datetime.datetime.now(), description = desc) #Because black is nice.
+    em.set_image(url = data['img']) #making embed
+    em.set_footer(text=f'Requested by {ctx.message.author.display_name}')
+    return em
+
+async def latest():
+    async with bot.session.get('https://xkcd.com/info.0.json') as resp: #getting data
+        print(resp.status)
+        data = await resp.json() #Pulling data
+    num = int(data['num'])
+    return num
+
 bot.run(TOKEN)
 #Made by @Extr3mis#9663
